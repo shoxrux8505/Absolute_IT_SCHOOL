7c2591f3d25079e6db5993b1dfe02869aaf2b5bc
Bettee0885 <b44786280@gamil.com>
2024-09-26 14:38:51 +0300

    done and added some changes

--- src/main.js
+++ src/main.js
@@ -2,17 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import PrimeVue from "primevue/config";
 // import Aura from '@primevue/themes/aura';
 import i18n from './TranslateI18';
 
 const app = createApp(App)
-// app.use(PrimeVue, {
-//     theme: {
-//         preset: Aura
-//     }
-// });
+
 app.use(router)
 app.use(i18n);
 app.mount('#app')
